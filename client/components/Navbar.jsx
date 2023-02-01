83a29aec94181fe9b441be629f559dea79390446
Noah Lett <noahblett96@gmail.com>
2023-02-01 20:18:53 +0000

    Added state to Navbar component and created new button component with corresponding CSS

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <div>
       <nav className="navbar">
@@ -9,6 +15,26 @@ export default function Navbar() {
           <Link to="/" className="navbar-logo">
             NL
           </Link>
+          <div className="menu-icon">
+            <i onClick={handleClick} className={click ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/connect' className='nav-links' onClick={closeMobileMenu}>
+                Connect
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
