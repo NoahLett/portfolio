19dd85dfcd9d16bacd05563d9b0741a182985ce1
Noah Lett <noahblett96@gmail.com>
2023-02-06 22:26:18 +0000

    Made further revisions of card and deck component, adding new props

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Card(props) {
-  const { imageUrl, name, shortDescription } = props.card;
 
   return (
-    <div>
-      <Link to={`/${name}`}>
-        <img src={imageUrl} alt={name} className='card-img' />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{shortDescription}</p>
+    <li className='deck-item'>
+      <Link className='deck-item-link' to={props.path}>
+        <div className='deck-item-pic-wrap'>
+          <img src={props.src} alt='text' className='deck-item-img' />
+        </div>
+        <div className="deck-item-info">
+          <h5 className="deck-item-name">{props.name}</h5>
+          <p className="deck-item-name">{props.text}</p>
+          <p className='deck-item-description'>Technologies Used:</p>
+          <p className='deck-item-list'>{props.techList}</p>
         </div>
       </Link>
-    </div>
+    </li>
   );
 }
--- client/components/Deck.jsx
+++ client/components/Deck.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import './Deck.css';
 
 export default function Deck() {
   return (
@@ -8,7 +9,18 @@ export default function Deck() {
       <div className="deck-container">
         <div className="deck-wrapper">
           <ul className="deck-items">
-            <Card/>
+            <Card
+              src='images/bookworm.png'
+              name='BookWorm'
+              text='A full-stack React application for college student looking to save money on overpriced college textbooks.'
+              path='https://book-worm.noahlett.com'
+              techList='' />
+            <Card
+              src='images/beerbase.png'
+              name='The Beer Base'
+              text='A dynamic JavaScript, HTML, and CSS application for homebrewers looking to try new recipes and post their batches.'
+              path='https://noahlett.github.io/beer-base/'
+              techList='JavaScript ES5' />
           </ul>
         </div>
       </div>
