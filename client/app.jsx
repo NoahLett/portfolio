5b984652289aeb05712db37dc9d3f66d46f4f3a2
Noah Lett <noahblett96@gmail.com>
2023-02-08 20:27:12 +0000

    Added routes for about and connect pages

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import About from './pages/about';
+import Connect from './pages/connect';
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/connect' element={<Connect />} />
         </Routes>
       </Router>
     </div>
