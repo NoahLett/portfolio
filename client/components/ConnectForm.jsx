1cd46873e66d0ca09d5ae04cbe0058d2392137af
Noah Lett <noahblett96@gmail.com>
2023-02-20 18:57:41 +0000

    Added connect form skeleton html and integrated component into connect page

--- client/components/ConnectForm.jsx
+++ client/components/ConnectForm.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 export default function ConnectForm() {
   return (
     <div>
-      <h1>Test</h1>
+      <form>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            required
+            autoFocus
+            type="text"
+            name='firstname' />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            required
+            type="text"
+            name='lastName' />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            required
+            type="email"
+            name='email' />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <input
+            required
+            type="textarea"
+            name='message' />
+        </div>
+        <div>
+          <input type="submit" value='Send!' />
+        </div>
+      </form>
     </div>
   );
 }
