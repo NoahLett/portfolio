98aae276515ba57faf0f91c1df8e1965aa8766dc
Noah Lett <noahblett96@gmail.com>
2023-02-22 18:51:47 +0000

    Added email form component to connect form jsx

--- client/components/ConnectForm.jsx
+++ client/components/ConnectForm.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Connect() {
+const EmailForm = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    fetch('/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, message })
+    })
+      .then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+      </label>
+      <label>
+        Message:
+        <textarea value={message} onChange={e => setMessage(e.target.value)} required />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
+
+export default EmailForm;
