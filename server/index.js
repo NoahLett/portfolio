55e287fbdc59f45fe4681d5fcb6c153accf74cee
Noah Lett <noahblett96@gmail.com>
2023-02-25 22:58:13 +0000

    Removed sendgrid cors and other unnecessary installs, removed server side code for sendgrid, and removed client side fetch request

--- server/index.js
+++ server/index.js
@@ -2,39 +2,11 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
-const bodyParser = require('body-parser');
-const sgMail = require('@sendgrid/mail');
-const cors = require('cors');
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(staticMiddleware);
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-app.post('/send-email', (req, res, next) => {
-  const { topic, message } = req.body;
-
-  const msg = {
-    to: 'noahblett96@gmail.com',
-    from: 'nblportfolio@gmail.com',
-    subject: topic,
-    text: message,
-    html: `<h1/>${message}</h1>`
-  };
-
-  sgMail.send(msg)
-    .then(result => {
-      res.status(200).json('Email Sent');
-    })
-    .catch(err => {
-      console.error(err);
-      next(err);
-    });
-});
 
 app.use(errorMiddleware);
 
