a1ec6e8dabcca1f0c5aaf4ec48b348d8a42dd7fb
Noah Lett <noahblett96@gmail.com>
2023-02-26 08:03:03 +0000

    Installed emailjs and incorporated send function into connect form react function component

--- client/components/ConnectForm.jsx
+++ client/components/ConnectForm.jsx
@@ -1,10 +1,21 @@
 import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
   const form = useRef();
 
   const sendEmail = e => {
     e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then(result => {
+        // eslint-disable-next-line no-console
+        console.log(result.text);
+      }, error => {
+        // eslint-disable-next-line no-console
+        console.log(error.text);
+      });
   };
 
   return (
