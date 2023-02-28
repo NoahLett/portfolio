f17a44abc6bcf7f7dfb25ef37fda57fa23677b77
Noah Lett <noahblett96@gmail.com>
2023-02-28 20:15:20 +0000

    Added social media icons and included corresponding CSS

--- client/components/SocialMedia.jsx
+++ client/components/SocialMedia.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaFacebook } from 'react-icons/fa';
+import './SocialMedia.css';
 
 export default function SocialMedia() {
   return (
-    <div>
-      <span>
-        <i className="fa-brands fa-linkedin" />
+    <div className='socialmedia-block'>
+      <span className="socialmedia-span">
+        <FaLinkedin className='socialmedia-icon' id='linkedin'/>
+        <FaGithub className='socialmedia-icon' id='github'/>
+        <FaFacebook className='socialmedia-icon' id='facebook'/>
       </span>
     </div>
   );
