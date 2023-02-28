c5ac07e43a05f986feccf1af77a310a9862133c5
Noah Lett <noahblett96@gmail.com>
2023-02-28 19:14:31 +0000

    Created basic socialmedia-links component and added component to connect page

---
+++ client/components/SocialMedia.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SocialMedia() {
+  return (
+    <div>
+      <span>
+        <i className="fa-brands fa-linkedin" />
+      </span>
+    </div>
+  );
+}
