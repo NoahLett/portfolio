b3e066b1d2f680d2c88aaed5e47278754ca6f96e
Noah Lett <noahblett96@gmail.com>
2023-03-02 21:10:44 +0000

    Improved layout of images in the tech list of about page

--- client/components/TechListItem.jsx
+++ client/components/TechListItem.jsx
@@ -4,7 +4,9 @@ import './TechListItem.css';
 export default function TechListItem(props) {
   return (
     <div className='card-item'>
-      <img className='card-img' src={props.image} alt={props.title} />
+      <div className='img-box'>
+        <img className='card-img' src={props.image} alt={props.title} />
+      </div>
       <div className="card-content">
         <h3 className='card-title'>{props.title}</h3>
         <p className='card-text'>{props.description}</p>
