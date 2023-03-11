6a3c73d70148094040d4ce375a27478d62e584d0
Noah Lett <noahblett96@gmail.com>
2023-03-11 19:17:45 +0000

    Made responsive layout changes to connect page

--- client/pages/connect.jsx
+++ client/pages/connect.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import ContactForm from '../components/ConnectForm';
 import SocialMedia from '../components/SocialMedia';
-
-const styles = {
-  connectHeader: {
-    marginTop: '8rem',
-    marginBottom: '3rem',
-    textAlign: 'center',
-    color: '#fff',
-    fontFamily: 'Trebuchet MS',
-    fontWeight: 'bold',
-    fontSize: '4rem'
-  }
-};
+import './connect.css';
 
 export default function Connect() {
   return (
     <div>
-      <h1 style={styles.connectHeader}>{'Let\'s Connect'}</h1>
+      <h1 className='connect-header'>{'Let\'s Connect'}</h1>
       <ContactForm />
       <SocialMedia />
     </div>
